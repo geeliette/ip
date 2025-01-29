fcb63ef58ae26bef3358c7318583625282f530c0
Juliette <geeliette@gmail.com>
2025-01-30 02:21:07 +0800

    Add task storing

--- src/main/java/Fleur.java
+++ src/main/java/Fleur.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class Fleur {
+    private static String[] list = new String[100];
+    private static int count = 0;
+
     public static void main(String[] args) {
         System.out.println("'Allo! I'm Fleur.");
-        System.out.println("What can I do for you?");
+        System.out.println("Tell me what you need to do, s'il vous plaît.");
+
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNextLine()) {
@@ -11,8 +15,14 @@ public class Fleur {
             if (str.equalsIgnoreCase("bye")) {
                 System.out.println("Au revoir, 'ope to see you again soon!");
                 return;
+            } else if (str.equalsIgnoreCase("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
             } else {
-                System.out.println(str);
+                list[count] = str;
+                count++;
+                System.out.println("added: " + str);
             }
         }
     }
