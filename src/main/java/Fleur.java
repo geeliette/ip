81cda7593a63cf1668049e632ea51172bd031a87
Juliette <geeliette@gmail.com>
2025-01-30 12:27:15 +0800

    Add Task class

--- src/main/java/Fleur.java
+++ src/main/java/Fleur.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Fleur {
-    private static String[] list = new String[100];
-    private static int count = 0;
+    private static Task[] tasks = new Task[100]; // array of tasks
+    private static int count = 0; // number of tasks
 
     public static void main(String[] args) {
         System.out.println("'Allo! I'm Fleur.");
@@ -12,15 +12,33 @@ public class Fleur {
 
         while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
+            // input is bye
             if (str.equalsIgnoreCase("bye")) {
                 System.out.println("Au revoir, 'ope to see you again soon!");
                 return;
+            // input is list
             } else if (str.equalsIgnoreCase("list")) {
+                System.out.println("'Ere are all ze tasks in your list: ");
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                    System.out.println((i + 1) + ". " + tasks[i].toString());
                 }
+            // input is mark task as done
+            } else if (str.startsWith("mark")) {
+                // get task number
+                int index = Integer.parseInt(str.substring(5)) - 1;
+                tasks[index].markAsDone();
+                System.out.println("Enchanté! I 'ave marked zis task as done:");
+                System.out.println(tasks[index].toString());
+            // input is mark task as undone
+            } else if (str.startsWith("unmark")) {
+                int index = Integer.parseInt(str.substring(7)) - 1;
+                tasks[index].markAsUndone();
+                System.out.println("Zut! I 'ave marked zis task as not done:");
+                System.out.println(tasks[index].toString());
+            // input is task
             } else {
-                list[count] = str;
+                Task t = new Task(str);
+                tasks[count] = t;
                 count++;
                 System.out.println("added: " + str);
             }
